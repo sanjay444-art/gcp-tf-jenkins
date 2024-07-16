762b1043715e2c3be95770d77397ddc6e9909a6b
sanjay444-art <sanjay.siddi@wipro.com>
2024-07-16 14:43:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
   name                     = "my-unique-bucket-name"
-  project                  = "my-project"
+  project                  = "uhg-poc-env"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
