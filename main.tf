bd8aa823ed278b0c3176f972d01e456893af3b76
sanjay444-art <sanjay.siddi@wipro.com>
2024-07-23 18:20:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket-demo" {
-  name                     = "tt-githubdemo-bucket-0011"
+  name                     = "tt-githubdemo-bucket-0012"
   project                  = "uhg-poc-env"
   location                 = "US"
   force_destroy            = true
